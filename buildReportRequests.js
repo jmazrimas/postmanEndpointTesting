506788e4e6559855b36305063e72c9037861dc84
Joe Mazrimas <joe.mazrimas@gmail.com>
2018-01-31 09:56:58 -0600

    parsing report list

---
+++ buildReportRequests.js
@@ -0,0 +1,5 @@
+module.exports = {
+  returnRequests: function(reportData) {
+      console.log(reportData)
+  }
+}
--- createPostmanImport.js
+++ createPostmanImport.js
@@ -1,37 +1,47 @@
 var fs = require('fs');
 var buildEndpoints = require('./buildEndpoints')
+var buildReportRequests =  require('./buildReportRequests')
 var writeMissingEndpoints = require('./writeMissingEndpoints')
 
-var readPostmanFile = function() {
-  var rawData = fs.readFileSync( __dirname + '/marcin.postman_collectionV2.json')
+
+
+// var readPostmanFile = function() {
+//   var rawData = fs.readFileSync( __dirname + '/marcin.postman_collectionV2.json')
+//   return JSON.parse(rawData)
+// }
+
+var readReportList = function() {
+  var rawData = fs.readFileSync( __dirname + '/reportList.json')
   return JSON.parse(rawData)
 }
 
-var psFile = readPostmanFile()
+buildReportRequests.returnRequests(readReportList());
 
-var endpointList = buildEndpoints.returnUniqueEndpoints(psFile,'dev-web2')
-
-var postmanImportShell = {
-	"variables": [],
-	"info": {
-		"name": "PostmanImport",
-		"_postman_id": "0266b793-8efc-b01d-66fc-e98098fcc985",
-		"schema": "https://schema.getpostman.com/json/collection/v2.0.0/collection.json"
-	},
-	"item": [
-	]
-}
-
-postmanImportShell.item = endpointList
-
-var fileContents = JSON.stringify(postmanImportShell)
-
-fs.writeFile("PORTALpostman-import.json", fileContents, function(err) {
-    if(err) {
-        return console.log(err);
-    }
-
-    console.log("Write import successful");
-});
+// var psFile = readReportList()
+//
+// var endpointList = buildEndpoints.returnUniqueEndpoints(psFile,'dev-web2')
+//
+// var postmanImportShell = {
+// 	"variables": [],
+// 	"info": {
+// 		"name": "PostmanImport",
+// 		"_postman_id": "0266b793-8efc-b01d-66fc-e98098fcc985",
+// 		"schema": "https://schema.getpostman.com/json/collection/v2.0.0/collection.json"
+// 	},
+// 	"item": [
+// 	]
+// }
+//
+// postmanImportShell.item = endpointList
+//
+// var fileContents = JSON.stringify(postmanImportShell)
+//
+// fs.writeFile("PORTALpostman-import.json", fileContents, function(err) {
+//     if(err) {
+//         return console.log(err);
+//     }
+//
+//     console.log("Write import successful");
+// });
 
 // writeMissingEndpoints.writeMissing(postmanImportShell)
