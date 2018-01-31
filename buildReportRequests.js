09d4372ee65b1a2faf5b6140fbc1dc7ed87c6f41
Joe Mazrimas <joe.mazrimas@gmail.com>
2018-01-31 10:12:54 -0600

    generating un-tested collection that looks correct

--- buildReportRequests.js
+++ buildReportRequests.js
@@ -1,5 +1,30 @@
+var requestTemplate = function(uuid) {
+  return {
+    "name": "url-to-pdf-api "+uuid,
+    "request": {
+      "url": "http://localhost:9000/api/render",
+      "method": "POST",
+      "header": [
+        {
+          "key": "Content-Type",
+          "value": "application/json",
+          "description": ""
+        }
+      ],
+      "body": {
+        "mode": "raw",
+        "raw": "{\n\t\"url\": \"https://app.amper.xyz/#/reports/"+uuid+"/auth/a78d1e7176f34e318f9f68770c03973e\",\n\t\"waitFor\": 5000,\n\t\"pdf\": {\n\t\t\"format\": \"Letter\"\n\t}\n}"
+      },
+      "description": ""
+    },
+    "response": []
+  }
+}
+
 module.exports = {
-  returnRequests: function(reportData) {
-      console.log(reportData)
+  returnRequests: function(reportData, limit) {
+    return reportData.data.reports.map((report) => {
+      return requestTemplate(report.uuid);
+    }).slice(0, limit || reportData.length)
   }
 }
--- createPostmanImport.js
+++ createPostmanImport.js
@@ -15,33 +15,33 @@ var readReportList = function() {
   return JSON.parse(rawData)
 }
 
-buildReportRequests.returnRequests(readReportList());
+// console.log(buildReportRequests.returnRequests(readReportList()));
 
 // var psFile = readReportList()
-//
+
 // var endpointList = buildEndpoints.returnUniqueEndpoints(psFile,'dev-web2')
-//
-// var postmanImportShell = {
-// 	"variables": [],
-// 	"info": {
-// 		"name": "PostmanImport",
-// 		"_postman_id": "0266b793-8efc-b01d-66fc-e98098fcc985",
-// 		"schema": "https://schema.getpostman.com/json/collection/v2.0.0/collection.json"
-// 	},
-// 	"item": [
-// 	]
-// }
-//
-// postmanImportShell.item = endpointList
-//
-// var fileContents = JSON.stringify(postmanImportShell)
-//
-// fs.writeFile("PORTALpostman-import.json", fileContents, function(err) {
-//     if(err) {
-//         return console.log(err);
-//     }
-//
-//     console.log("Write import successful");
-// });
+
+var postmanImportShell = {
+	"variables": [],
+	"info": {
+		"name": "PostmanImport",
+		"_postman_id": "0266b793-8efc-b01d-66fc-e98098fcc985",
+		"schema": "https://schema.getpostman.com/json/collection/v2.0.0/collection.json"
+	},
+	"item": [
+	]
+}
+
+postmanImportShell.item = buildReportRequests.returnRequests(readReportList(), 5)
+
+var fileContents = JSON.stringify(postmanImportShell)
+
+fs.writeFile("PDFs-postman-import.json", fileContents, function(err) {
+    if(err) {
+        return console.log(err);
+    }
+
+    console.log("Write import successful");
+});
 
 // writeMissingEndpoints.writeMissing(postmanImportShell)
